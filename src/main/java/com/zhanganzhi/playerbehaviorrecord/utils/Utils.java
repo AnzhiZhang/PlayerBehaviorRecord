88157dbfddc533689b98fa9ba5055dc2a70ad648
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-02-02 16:18:01 +0000

    docs: 💡 add docstring in Utils

--- src/main/java/com/zhanganzhi/playerbehaviorrecord/utils/Utils.java
+++ src/main/java/com/zhanganzhi/playerbehaviorrecord/utils/Utils.java
@@ -3,6 +3,12 @@ package com.zhanganzhi.playerbehaviorrecord.utils;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class Utils {
+    /**
+     * Check if the player is a bot.
+     *
+     * @param serverPlayerEntity the player to check.
+     * @return true if the player is a bot, false otherwise.
+     */
     public static boolean isBot(ServerPlayerEntity serverPlayerEntity) {
         return serverPlayerEntity.networkHandler.getConnection().getAddress() == null;
     }
